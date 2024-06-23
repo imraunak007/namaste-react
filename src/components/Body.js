a9c817e9a67af121acb64fe1f11600c9aa7007ea
Raunak Mittal <raunakmittal@Raunaks-MacBook-Air.local>
2024-06-23 14:12:55 +0530

    episode-10

--- src/components/Body.js
+++ src/components/Body.js
@@ -40,12 +40,12 @@ const Body = () => {
 
     return listOfRestaurants.length === 0 ? <Shimmer /> : (
         <div className="body">
-            <div className="filter">
-                <div className="search">
-                    <input type="text" className="search-box" value={searchText} onChange={(e) => {
+            <div className="flex">
+                <div className="p-4 m-4">
+                    <input type="text" className="border border-black border-solid" value={searchText} onChange={(e) => {
                         setSearchText(e.target.value);
                     }} />
-                    <button onClick={() => {
+                    <button className="px-4 py-2 m-4 bg-green-100 rounded-lg" onClick={() => {
                         const filteredRestaurant = listOfRestaurants.filter((res) => {
                             return res?.info?.name.toLowerCase().includes(searchText.toLowerCase())
                         });
@@ -53,15 +53,17 @@ const Body = () => {
                         console.log(searchText);
                     }}>Search</button>
                 </div>
-                <button className="filter-btn" onClick={() => {
+                <div className="p-4 m-4 flex items-center">
+                <button className="px-4 py-2 m-4 bg-gray-100" onClick={() => {
                     const filteredList = listOfRestaurants.filter((res) => res?.info?.avgRating === 4.3);
                     console.log(filteredList);
                     setFilteredRestaurants(filteredList);
                 }}>
                     Top Rated Restaurants
                 </button>
+                </div>
             </div>
-            <div className="res-container">
+            <div className="flex flex-wrap">
                 {filteredRestaurants.map((restaurant) => {
                     return (<Link key={restaurant?.info?.id} to={"/restaurants/" + restaurant?.info?.id}><RestaurantCard resData={restaurant}/></Link>)
                 })}
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -20,9 +20,9 @@ const RestaurantCard = (props) => {
     const { deliveryTime } = sla;
 
     return (
-        <div className="res-card" style={styleCard}>
-            <img className="res-logo" alt="res-logo" src={CDN_URL+cloudinaryImageId} />
-            <h3>{name}</h3>
+        <div className="m-4 p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-400">
+            <img className=" rounded-lg" alt="res-logo" src={CDN_URL+cloudinaryImageId} />
+            <h3 className=" font-bold py-4 text-lg">{name}</h3>
             <h4>{cuisines.join(", ")}</h4>
             <h4>{avgRating} stars</h4>
             <h4>{costForTwo}</h4>
